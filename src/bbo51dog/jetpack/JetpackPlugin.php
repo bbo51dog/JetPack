cba833a8aaffae7de3f5a4d4c5db8755745c1a66
bbo51dog <bbo51dog@gmail.com>
2020-02-27 22:57:37 +0900

    Create Item class

--- src/bbo51dog/jetpack/JetpackPlugin.php
+++ src/bbo51dog/jetpack/JetpackPlugin.php
@@ -2,8 +2,16 @@
 
 namespace bbo51dog\jetpack;
 
+use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
 use pocketmine\plugin\PluginBase;
+use bbo51dog\jetpack\item\JetPack;
 
 class JetPackPlugin extends PluginBase{
-    
+
+    public function onEnable(){
+        $jetpack = new JetPack(JetPack::ID, JetPack::META, JetPack::NAME);
+        ItemFactory::registerItem($jetpack);
+        Item::addCreativeItem($jetpack);
+    }
 }
---
+++ src/bbo51dog/jetpack/item/JetPack.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace bbo51dog\jetpack\item;
+
+use pocketmine\item\Armor;
+
+class JetPack extends Armor{
+
+    /** @var int */
+    public const ID = 600;
+
+    /** @var int */
+    public const META = 0;
+
+    /** @var string */
+    public const NAME = 'Jet Pack';
+}
