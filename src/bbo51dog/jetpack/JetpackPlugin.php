922607c7961b84a289aafe1a65a3d5fd631b54ec
bbo51dog <bbo51dog@gmail.com>
2020-02-28 10:25:07 +0900

    Create recipe

--- src/bbo51dog/jetpack/JetpackPlugin.php
+++ src/bbo51dog/jetpack/JetpackPlugin.php
@@ -2,16 +2,35 @@
 
 namespace bbo51dog\jetpack;
 
+use pocketmine\inventory\ShapedRecipe;
 use pocketmine\item\Item;
 use pocketmine\item\ItemFactory;
 use pocketmine\plugin\PluginBase;
 use bbo51dog\jetpack\item\JetPack;
+use bbo51dog\jetpack\item\JetPackFactory;
 
 class JetPackPlugin extends PluginBase{
 
     public function onEnable(){
         $jetpack = new JetPack();
-        ItemFactory::registerItem($jetpack);
-        Item::addCreativeItem($jetpack);
+        JetPackFactory::register($jetpack);
+        //Item::addCreativeItem($jetpack);
+
+        $recipe = new ShapedRecipe(
+            [
+                'iai',
+                'iii',
+                'ccc',
+            ],
+            [
+                'i' => Item::get(Item::IRON_INGOT),
+                'a' => Item::get(Item::AIR),
+                'c' => Item::get(Item::COAL),
+            ],
+            [
+                JetPackFactory::get(JetPack::ID),
+            ]
+        );
+        $this->getServer()->getCraftingManager()->registerShapedRecipe($recipe);
     }
 }
