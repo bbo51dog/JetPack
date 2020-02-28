c4129e9216e9de8f056e4a37c1237982161a1290
bbo51dog <bbo51dog@gmail.com>
2020-02-28 20:27:12 +0900

    particle

--- src/bbo51dog/jetpack/EventListener.php
+++ src/bbo51dog/jetpack/EventListener.php
@@ -2,13 +2,22 @@
 
 namespace bbo51dog\jetpack;
 
+use bbo51dog\jetpack\task\ParticleTask;
 use pocketmine\item\Item;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJumpEvent;
 use pocketmine\math\Vector3;
 use pocketmine\nbt\tag\CompoundTag;
-use pocketmine\event\player\PlayerJumpEvent;
+use pocketmine\scheduler\TaskScheduler;
 
 class EventListener implements Listener{
+
+    /** @var TaskScheduler */
+    private $scheduler;
+
+    public function __construct(TaskScheduler $scheduler){
+        $this->scheduler = $scheduler;
+    }
 
     public function onJump(PlayerJumpEvent $event){
         $player = $event->getPlayer();
@@ -21,6 +30,7 @@ class EventListener implements Listener{
             return;
         }
         $vector = $player->getDirectionVector();
-        $player->setMotion(new Vector3($vector->x * 0.1 , 0.8, $vector->z * 0.1));
+        $player->setMotion(new Vector3($vector->x * 0.1 , 0.9, $vector->z * 0.1));
+        $this->scheduler->scheduleRepeatingTask(new ParticleTask($player), 2);
     }
 }
