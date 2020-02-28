c31ed04e5015bdfa492929c2ff59be2db9d8de12
bbo51dog <bbo51dog@gmail.com>
2020-02-28 16:37:58 +0900

    Create JetPackCommand

--- src/bbo51dog/jetpack/JetpackPlugin.php
+++ src/bbo51dog/jetpack/JetpackPlugin.php
@@ -6,27 +6,13 @@ namespace bbo51dog\jetpack;
 use pocketmine\inventory\ShapedRecipe;
 use pocketmine\item\Item;
 use pocketmine\plugin\PluginBase;
+use bbo51dog\jetpack\command\JetPackCommand;
 use bbo51dog\jetpack\item\JetPack;
 
 class JetPackPlugin extends PluginBase{
 
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
-        $recipe = new ShapedRecipe(
-            [
-                'iai',
-                'iii',
-                'ccc',
-            ],
-            [
-                'i' => Item::get(Item::IRON_INGOT),
-                'a' => Item::get(Item::AIR),
-                'c' => Item::get(Item::COAL),
-            ],
-            [
-                new JetPack(),
-            ]
-        );
-        $this->getServer()->getCraftingManager()->registerShapedRecipe($recipe);
+        $this->getServer()->getCommandMap()->register('jetpack', new JetPackCommand)
     }
 }
---
+++ src/bbo51dog/jetpack/command/JetPackCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace bbo51dog\jetpack\command;
+
+use pocketmine\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use bbo51dog\jetpack\item\JetPack;
+
+class JetPackCommand extends Command{
+
+    private const PREFIX = '§6[§cJetPack§6]§r ';
+
+    public function __construct(){
+        $this->setPermission('jetpack.command');
+        parent::__construct("jetpack", "Give JetPack Command", "/jetpack");
+    } 
+    public function execute(CommandSender $sender, string $commandLabel, array $args){
+        if(!$sender instanceof Player){
+            $sender->sendMessage(self::PREFIX.'§cサーバー内で実行してください');
+            return;
+        }
+        $inventory = $sender->getInventory();
+        $jetpack = new JetPack();
+        if($inventory->canAddItem($jetpack)){
+            if($inventory->getItemInHand()->getId === JetPack::AIR){
+                $inventory->setItemInHand($jetpack);
+            }else{
+                $inventory->addItem($jetpack);
+            }
+            $sender->sendMessage(self::PREFIX.'JetPackを付与しました');
+        }else{
+            $sender->sendMessage(self::PREFIX.'インベントリにアイテムを追加できません');
+        }
+    }
+}
